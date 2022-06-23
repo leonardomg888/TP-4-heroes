77c7ae3df867b01c22c2268d8eaf7e689467ed7a
leonardomg888 <leonardomg888@gmail.com>
2022-06-23 06:06:21 +0200

    imagenes

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express();
 const puerto = 3030;
 
 //console.log(__dirname);
+app.use(express.static('public'));
+
 
 app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views', 'index.html')));
 app.get('/babbage', (req,res) => res.sendFile(path.join(__dirname, 'views', 'babbage.html')));
